b4b936e1697b6c2c1d55fbcf9f5846af19c941f6
Zavier <2104599B@student.tp.edu.sg>
2022-12-14 22:10:49 +0800

    coded to select data from SQL and send it over to cart.jsp

--- src/main/java/CartServlet.java
+++ src/main/java/CartServlet.java
@@ -21,7 +21,7 @@ public class CartServlet extends HttpServlet {
 	private String jdbcURL = "jdbc:mysql://localhost:3306";
 	private String jdbcUsername = "root";
 	private String jdbcPassword = "cdev";
-	private static final String SELECT_ALL_FOOD_FROM_USER_CART = "SELECT cart.cart_id,food.food_id,food.name,food.price,food.description from food Inner JOIN cart on food.food_id = cart.cart_food_id where cart.cart_user_id = 2;;";
+	private static final String SELECT_ALL_FOOD_FROM_USER_CART = "SELECT freshfoodies.cart.cart_id, freshfoodies.food.food_id, freshfoodies.food.name, freshfoodies.food.price, freshfoodies.food.description from freshfoodies.food Inner JOIN freshfoodies.cart on freshfoodies.food.food_id = freshfoodies.cart.cart_food_id where freshfoodies.cart.cart_user_id = ?;";
 	private static final long serialVersionUID = 1L;
 	
 	protected Connection getConnection() {
@@ -41,7 +41,7 @@ public class CartServlet extends HttpServlet {
         super();
     }
     
-    private void listFoods(HttpServletRequest request, HttpServletResponse response)
+    private void listCarts(HttpServletRequest request, HttpServletResponse response)
 			throws SQLException, IOException, ServletException {
 		if (request.getAttribute("loggedin") != null) {
 			boolean loggedin = (boolean) request.getAttribute("loggedin");
@@ -50,23 +50,25 @@ public class CartServlet extends HttpServlet {
 			int user_id = (int) request.getAttribute("user_id");
 		}
 
-		List<Food> foods = new ArrayList<>();
+		List<UserCart> carts = new ArrayList<>();
 		try (Connection connection = getConnection();
 				PreparedStatement preparedStatement = connection.prepareStatement(SELECT_ALL_FOOD_FROM_USER_CART);) {
+			preparedStatement.setInt(1, 2);
 			ResultSet rs = preparedStatement.executeQuery();
 			while (rs.next()) {
+				int cart_id = rs.getInt("cart_id");
 				int food_id = rs.getInt("food_id");
 				String name = rs.getString("name");
 				double price = rs.getDouble("price");
 				String description = rs.getString("description");
-				String img = rs.getString("img");
-				foods.add(new Food(food_id, name, price, description, img));
+				carts.add(new UserCart(cart_id,food_id, name, price, description));
 			}
 		} catch (SQLException e) {
+			System.out.println("Tester");
 			System.out.println(e.getMessage());
 		}
-		request.setAttribute("listFoods", foods);
-		request.getRequestDispatcher("/index.jsp").forward(request, response);
+		request.setAttribute("listCarts", carts);
+		request.getRequestDispatcher("/cart.jsp").forward(request, response);
 	}
 
 	/**
@@ -75,6 +77,12 @@ public class CartServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
+		try {
+			listCarts(request, response);
+		} catch (SQLException ex) {
+			throw new ServletException(ex);
+		}
+		
 	}
 
 	/**
