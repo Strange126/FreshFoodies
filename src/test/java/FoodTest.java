4d95b1c0be2873f72f7c40adde30585fc297e74a
Strange126 <Bernielee126@gmail.com>
2022-12-20 18:26:16 +0800

    Added FoodTest.java to run test on the FoodClass

---
+++ src/test/java/FoodTest.java
@@ -0,0 +1,72 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class FoodTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testFood() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetFood_id() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetFood_id() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetPrice() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetPrice() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetDescription() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetDescription() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetImg() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetImg() {
+		fail("Not yet implemented");
+	}
+
+}
